4ce94ee89277a9d912e82bc46b4a06fb7dba30ec
jedrzejkozal <jedrzej.kozal@gmail.com>
2025-05-08 16:43:47 +0200

    fix bcsr implementation for tinyimagenet

--- models/bcsr.py
+++ models/bcsr.py
@@ -22,18 +22,23 @@ def get_parser() -> ArgumentParser:
 
 
 class SimpleCNN(nn.Module):
-    def __init__(self, n_classes):
+    def __init__(self, n_classes, larger_input=False):
         super(SimpleCNN, self).__init__()
+        self.larger_input = larger_input
         self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
         self.pool = nn.MaxPool2d(2)
         self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
-        self.fc1 = nn.Linear(64 * 8 * 8, 128)
+        output_size = 64 * 16 * 16 if larger_input else 64 * 8 * 8
+        self.fc1 = nn.Linear(output_size, 128)
         self.fc2 = nn.Linear(128, n_classes)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 64 * 8 * 8)
+        if self.larger_input:
+            x = x.view(-1, 64 * 16 * 16)
+        else:
+            x = x.view(-1, 64 * 8 * 8)
         x = F.relu(self.fc1(x))
         return self.fc2(x)
 
@@ -246,7 +251,7 @@ class Bcsr(ContinualModel):
         X_task = torch.cat(X_task)
         y_task = torch.cat(y_task)
 
-        model = SimpleCNN(dataset.N_CLASSES).to(device)
+        model = SimpleCNN(dataset.N_CLASSES, larger_input=dataset.NAME == 'seq-tinyimg').to(device)
 
         selector = BCSR(model, lr_proxy_model=0.01, beta=1.0, device=device)
         selected_indices, _ = selector.select_coreset(model, X_task.to(device), y_task.to(device), task_id, topk=buffer_size // (task_id + 1))
