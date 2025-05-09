bd3d66143527b0666fb6fe3e0485fa6b0ac28457
jedrzejkozal <jedrzej.kozal@gmail.com>
2025-05-09 07:55:22 +0200

    update memorization plots

--- plots_memorization.py
+++ plots_memorization.py
@@ -7,9 +7,26 @@ os.environ['QT_XCB_GL_INTEGRATION'] = 'none'
 
 def main():
     runs_dict = {
-        'er': ['63d06dda0258480e9275b18a61c3e7a0', '54a8c4be08874adcb7cfa59290322f8a', 'd2f3694cb4324520b684ea1445e279a3', '4287d76782b14e19815c6807c6d6c7be', 'd6e45200b3de4fcb9b80d65976c90751'],
-        'full': ['439874c1c8844572ab49f9aee1f1774d', '551998af45a44ebda533ea870ffb7983', '2f60ebcbc4ed499faaedc7005905f4b4', 'f737868dd28f4d058c6d294fd5d80e68', '10bfbbf7ee954e1b8495bea16a358e1c'],
-        'lwf': ['93e1907e382541b9a64ca40c626fcd5f', 'b045885a28174f39a6b761d92c2a26ab', 'ac269737061b4a159aba4a203c718966', '1bf5ddd62ca54847b10b37fd1089aca7', 'f02f61daa8f34fb7810099dbb8c74823'],
+        0.25: {
+            'er': ['63d06dda0258480e9275b18a61c3e7a0', '54a8c4be08874adcb7cfa59290322f8a', 'd2f3694cb4324520b684ea1445e279a3', '4287d76782b14e19815c6807c6d6c7be', 'd6e45200b3de4fcb9b80d65976c90751'],
+            'full': ['439874c1c8844572ab49f9aee1f1774d', '551998af45a44ebda533ea870ffb7983', '2f60ebcbc4ed499faaedc7005905f4b4', 'f737868dd28f4d058c6d294fd5d80e68', '10bfbbf7ee954e1b8495bea16a358e1c'],
+            'lwf': ['93e1907e382541b9a64ca40c626fcd5f', 'b045885a28174f39a6b761d92c2a26ab', 'ac269737061b4a159aba4a203c718966', '1bf5ddd62ca54847b10b37fd1089aca7', 'f02f61daa8f34fb7810099dbb8c74823'],
+        },
+        0.5: {
+            'er': ['60a84e62de034bb0bb0dbb328908bc52', 'f02defd31e8b4cf99bc0754ba9a4129e',],  # '3f4612b62ffb4a11902d1610a7ebcab0'],
+            'full': ['b979bc134d79483a91b0b5cd2800209d', 'c775e35167df4bde939ce37b63e0676e',],  # '8423b4441068402bbd3d4679bc0d6f50'],
+            'lwf': ['48a491ffc74e4c06a90d60084d8d4f5a', 'd9714d333aa34fc680d2f20a14883ecf',]  # '6bdc100a882b4980a16dc8ac7d2ccff2'],
+        },
+        0.75: {
+            'er': ['433322efe59442abbdaa90633c2cba22', '1e34f9c884e24f1db8d5a8fb50c5e59f',],
+            'full': ['65594a03a3634cb59d4a7ff97dc7a39f', 'b22827c12489416798e606669199868e',],
+            'lwf': ['7ca23ca5b07a4410a385861752d53a4b', '01a6c67df52e4a7eabf52500c9c2d368',]
+        },
+        0.9: {
+            'er': ['915ea3dcea9e400880c2a287fe5c51e0', 'f00c477580f348ebb85671bc5b963689',],
+            'full': ['a382b6d73c434c1380b806eb39c161b8', '36750086f8a84fb99f6827ea6416994f',],
+            'lwf': ['e702c66202f04165ab80503f9f784bc5', 'e065e93d788345099c63c87e40b118da',]
+        },
     }
     run_experiments = {
         'er': '976651693442159172',
@@ -17,11 +34,21 @@ def main():
         'lwf': '0',
     }
 
+    plt.figure()
+    plot_results(runs_dict[0.25], run_experiments)
+    run_experiments['er'] = '0'
+    plt.figure()
+    plot_results(runs_dict[0.5], run_experiments)
+    plt.figure()
+    plot_results(runs_dict[0.75], run_experiments)
+    plt.figure()
+    plot_results(runs_dict[0.9], run_experiments)
+    plt.show()
+
+
+def plot_results(runs_dict, run_experiments):
     algorithms = list(runs_dict.keys())
     n_tasks = 7
-
-    mlruns_path = '///home/jkozal/Documents/PWr/memorization-cl/memorization-cl/mlruns/'
-
     results = {a: [] for a in algorithms}
     results_std = {a: [] for a in algorithms}
     results_longtail = {a: [] for a in algorithms}
@@ -84,8 +111,6 @@ def main():
         plt.xlabel('Task')
         plt.ylabel('Accuracy')
         plt.legend()
-
-    plt.show()
 
 
 def get_task_acc(run_id, experiment_id, task_id):
