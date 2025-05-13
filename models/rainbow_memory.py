3e99b6755281e8ab8fa294ce70106357ac549fc9
Jedrzej Kozal <jedrzej.kozal@gmail.com>
2025-05-13 15:51:41 +0200

    update implementation

--- models/rainbow_memory.py
+++ models/rainbow_memory.py
@@ -4,8 +4,7 @@ import torch.nn.functional as F
 from torch.utils.data import DataLoader, TensorDataset, ConcatDataset
 import torch.nn as nn
 import numpy as np
-from copy import deepcopy
-from datasets.utils.additional_augmentations import CIFAR10Policy
+from torchvision.transforms import ToPILImage
 
 from models.utils.continual_model import ContinualModel
 from utils.args import add_management_args, add_experiment_args, add_rehearsal_args, ArgumentParser
@@ -24,46 +23,26 @@ def get_parser() -> ArgumentParser:
     return parser
 
 
-class SimpleCNN(nn.Module):
-    def __init__(self, n_classes, larger_input=False):
-        super(SimpleCNN, self).__init__()
-        self.larger_input = larger_input
-        self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
-        self.pool = nn.MaxPool2d(2)
-        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
-        output_size = 64 * 16 * 16 if larger_input else 64 * 8 * 8
-        self.fc1 = nn.Linear(output_size, 128)
-        self.fc2 = nn.Linear(128, n_classes)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        if self.larger_input:
-            x = x.view(-1, 64 * 16 * 16)
-        else:
-            x = x.view(-1, 64 * 8 * 8)
-        x = F.relu(self.fc1(x))
-        return self.fc2(x)
-
-
-class RainbowMemory:
-    def __init__(self, memory_size, num_augs, device="cpu", batch_size=128):
+class RainbowMemorySelector:
+    def __init__(self, memory_size, num_augs, augmentations, device="cpu", batch_size=128):
         self.memory_size = memory_size
         self.num_classes_so_far = 0
-        self.augmentations = CIFAR10Policy()  # RandAugment()
+        self.augmentations = augmentations
         self.num_augs = num_augs
         self.device = device
         self.classes = []
         self.batch_size = batch_size
+        self.to_pil = ToPILImage()
 
     def select_coreset(self, model, task_dataset, current_buffer_dataset):
-        new_classes = np.unique(task_dataset.targets).tolist()
+        new_classes = np.unique(task_dataset.dataset.targets).tolist()
         self.classes.extend([c for c in new_classes if c not in self.classes])
         combined_dataset = ConcatDataset([task_dataset, current_buffer_dataset])
-        targets = np.concatenate([np.array(d.targets) for d in combined_dataset.datasets])
+        targets = np.concatenate([np.array(d.dataset.targets) if type(d) == DatasetWrapper else np.array(d.targets.cpu()) for d in combined_dataset.datasets])
         combined_dataloader = DataLoader(combined_dataset, batch_size=self.batch_size, shuffle=False)
         u = self.calculate_u(model, combined_dataloader)
         k_c = int(self.memory_size / len(self.classes))
+        rest = self.memory_size % len(self.classes)
         new_buffer_x, new_buffer_y = [], []
         for c in self.classes:
             D_c_idxs = np.where(targets == c)[0]
@@ -72,17 +51,20 @@ class RainbowMemory:
             D_x_idxs_sorted = sorted(D_c_idxs, key=lambda idx: u[idx])
             x_selected_c, y_selected_c = [], []
 
-            for j in range(min(k_c, len(D_c_idxs))):
-                i = D_x_idxs_sorted[int(j * len(D_c_idxs) / k_c)]
+            size = k_c + 1 if c < rest else k_c
+            for j in range(min(size, len(D_c_idxs))):
+                i = D_x_idxs_sorted[int(j * len(D_c_idxs) / size)]
                 x, y = combined_dataset[i]
                 x_selected_c.append(x)
-                y_selected_c.append(c)
+                y_selected_c.append(y)
             new_buffer_x.extend(x_selected_c)
             new_buffer_y.extend(y_selected_c)
-        return TensorDataset(torch.stack(new_buffer_x), torch.tensor(new_buffer_y))
+        # return TensorDataset(torch.stack(new_buffer_x), torch.tensor(new_buffer_y))
+        return torch.stack(new_buffer_x), torch.tensor(new_buffer_y)
 
     def calculate_u(self, model, dataloader):
         dataset_len = len(dataloader.dataset)
+        n_classes = len(self.classes)
         u_matrix = np.zeros((dataset_len, len(self.classes)))
 
         model.eval()
@@ -92,9 +74,12 @@ class RainbowMemory:
             x_batch = x_batch.to(self.device)
 
             for _ in range(self.num_augs):
-                aug_x = self.augmentations(x_batch)
+                # aug_x = self.augmentations(x_batch)
+                aug_x = torch.stack([self.augmentations(self.to_pil(x)) for x in x_batch]).to(self.device)
                 with torch.no_grad():
-                    predictions = model(aug_x).argmax(dim=1).cpu().numpy()
+                    y_pred = model(aug_x)
+                    y_pred = y_pred[:, :n_classes]
+                    predictions = y_pred.argmax(dim=1).cpu().numpy()
 
                 for i, pred in enumerate(predictions):
                     if global_idx + i < dataset_len:
@@ -106,6 +91,18 @@ class RainbowMemory:
         return np.max(u_matrix, axis=1)
 
 
+class DatasetWrapper:
+    def __init__(self, dataset):
+        self.dataset = dataset
+
+    def __getitem__(self, i):
+        _, y, not_aug, _ = self.dataset[i]
+        return not_aug, y
+
+    def __len__(self):
+        return len(self.dataset)
+
+
 class RainbowMemory(ContinualModel):
     NAME = 'rainbow_memory'
     COMPATIBILITY = ['class-il', 'domain-il', 'task-il', 'general-continual']
@@ -114,6 +111,7 @@ class RainbowMemory(ContinualModel):
         super().__init__(backbone, loss, args, transform)
         self.buffer = Buffer(self.args.buffer_size, self.device, mode=args.buffer_policy)
         self.t = 0
+        self.selector = None
 
     def observe(self, inputs, labels, not_aug_inputs):
         real_batch_size = inputs.shape[0]
@@ -130,8 +128,9 @@ class RainbowMemory(ContinualModel):
         loss.backward()
         self.opt.step()
 
-        self.buffer.add_data(examples=not_aug_inputs,
-                             labels=labels[:real_batch_size])
+        if self.t == 0:
+            self.buffer.add_data(examples=not_aug_inputs,
+                                 labels=labels[:real_batch_size])
 
         return loss.item()
 
@@ -148,40 +147,48 @@ class RainbowMemory(ContinualModel):
         # X_task = torch.cat(X_task)
         # y_task = torch.cat(y_task)
 
-        model = SimpleCNN(dataset.N_CLASSES, larger_input=dataset.NAME == 'seq-tinyimg').to(device)
+        if self.selector is None:
+            augmentations = dataset.train_transform
+            self.selector = RainbowMemorySelector(self.args.buffer_size, 5, augmentations, device=device)
 
-        selector = RainbowMemory(self.args.buffer_size, 5, device=device)
-        selected_indices, _ = selector.select_coreset(model, dataset.train_loader.dataset, K=buffer_size // (task_id + 1))
+        buf_inputs, buf_labels = self.buffer.get_data(len(self.buffer), transform=self.transform)
+        buffer_dataset = TensorDataset(buf_inputs.cpu(), buf_labels.cpu())
+        buffer_dataset.targets = buf_labels.cpu()
+        x, y = self.selector.select_coreset(self.net, DatasetWrapper(dataset.train_loader.dataset), buffer_dataset)
+        assert len(x) == self.args.buffer_size
+        assert len(y) == self.args.buffer_size
+        self.buffer.examples = x.to(self.device)
+        self.buffer.labels = y.to(self.device)
 
-        added_labels = []
+        # added_labels = []
 
-        train_dataset = dataset.train_loader.dataset
+        # train_dataset = dataset.train_loader.dataset
 
-        if self.t == 0:
-            for buffer_idx, dataset_idx in enumerate(selected_indices):
-                _, label, not_aug_img, _ = train_dataset[dataset_idx]
+        # if self.t == 0:
+        #     for buffer_idx, dataset_idx in enumerate(selected_indices):
+        #         _, label, not_aug_img, _ = train_dataset[dataset_idx]
 
-                self.buffer.examples[buffer_idx] = not_aug_img
-                self.buffer.labels[buffer_idx] = label
-                added_labels.append(label.item())
-        else:
-            for dataset_idx in selected_indices:
-                _, label, not_aug_img, _ = train_dataset[dataset_idx]
+        #         self.buffer.examples[buffer_idx] = not_aug_img
+        #         self.buffer.labels[buffer_idx] = label
+        #         added_labels.append(label.item())
+        # else:
+        #     for dataset_idx in selected_indices:
+        #         _, label, not_aug_img, _ = train_dataset[dataset_idx]
 
-                buffer_classes, buffer_counts = torch.unique(self.buffer.labels, return_counts=True)
-                max_idx = torch.argmax(buffer_counts).item()
-                max_label = buffer_classes[max_idx]
-                buffer_class_idxs = torch.argwhere(self.buffer.labels == max_label).flatten()
-                buffer_idx = buffer_class_idxs[torch.randint(len(buffer_class_idxs), (1,)).item()]
+        #         buffer_classes, buffer_counts = torch.unique(self.buffer.labels, return_counts=True)
+        #         max_idx = torch.argmax(buffer_counts).item()
+        #         max_label = buffer_classes[max_idx]
+        #         buffer_class_idxs = torch.argwhere(self.buffer.labels == max_label).flatten()
+        #         buffer_idx = buffer_class_idxs[torch.randint(len(buffer_class_idxs), (1,)).item()]
 
-                self.buffer.examples[buffer_idx] = not_aug_img
-                self.buffer.labels[buffer_idx] = label
-                added_labels.append(label.item())
+        #         self.buffer.examples[buffer_idx] = not_aug_img
+        #         self.buffer.labels[buffer_idx] = label
+        #         added_labels.append(label.item())
 
-        print()
-        print('labels added to the buffer')
-        u1, u2 = np.unique(added_labels, return_counts=True)
-        print(u1.tolist(), u2.tolist())
+        # print()
+        # print('labels added to the buffer')
+        # u1, u2 = np.unique(added_labels, return_counts=True)
+        # print(u1.tolist(), u2.tolist())
         print()
         print('all labels in the buffer:')
         u1, u2 = torch.unique(self.buffer.labels, return_counts=True)
